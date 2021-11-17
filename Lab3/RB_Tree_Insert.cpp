4a32e70fc675bbe61134e996e239c31d2027293b
Carry <a2294510419@163.com>
2021-11-17 14:01:14 +0800

    C++实现红黑树插入--finished

--- Lab3/RB_Tree_Insert.cpp
+++ Lab3/RB_Tree_Insert.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <queue>
+#include <string>
+#include <cstring>
+
 using namespace std;
 typedef bool colortype;
 const colortype black = false;
 const colortype red = true;
+
+typedef pair<int,vector<int> > Input;
 struct TNode{
     int key;
     TNode* left;
@@ -173,6 +179,7 @@ RBTree& RB_Insert(RBTree& T, TNode* z)
 }
 void LevelOrderTraverse(RBTree& T)
 {
+    ofstream outfile("LOT.txt");
     queue<TNode*> q;
     TNode* p;
     if(T.Root() != T.Nil())
@@ -180,19 +187,95 @@ void LevelOrderTraverse(RBTree& T)
     while(q.size())
     {
         p = q.front();
-        cout<<p->key<<" ";
+        if(p==T.Nil())
+            outfile<<"nil"<<endl;
+        else if(p->color==red)
+        {
+            outfile<<p->key<<", "<<"red"<<endl;
+        }
+        else if(p->color==black)
+        {
+            outfile<<p->key<<", "<<"black"<<endl;
+        }
         q.pop();
-        if(p->left&&p->left!=T.Nil())
+        if(p->left)
             q.push(p->left);
-        if(p->right&&p->right!=T.Nil())
+        if(p->right)
             q.push(p->right);
     }
 }
+
+vector<string> split(const string& str, const string& delim) //将输入字符串str按delim标志进行分割
+{
+    vector<string> vec_result;
+	if("" == str) return vec_result; //如果要分割的串为空串，那么不进行分割
+
+	//先将要切割的字符串从string类型转换为char*类型，之所以要这样做，是因为下面要调用的C库函数strtok的接口是这样要求的
+	char * strs = new char[str.length() + 1] ; 
+	strcpy(strs, str.c_str()); 
+    
+    //同上
+	char * d = new char[delim.length() + 1];
+	strcpy(d, delim.c_str());
+ 
+	char *p = strtok(strs, d); //调用C库函数strok()进行字符串分割
+	while(p) { 
+		string s = p; //分割得到的字符串转换为string类型
+		vec_result.push_back(s); //存入结果数组
+		p = strtok(NULL, d); //这里之所以要这样写是因为strtok函数是多次调用的，详情见CSDN
+	}
+ 
+	return vec_result;
+}
+
+vector<vector<int> > Read(){ //读取txt文件，处理成一个vector<vector<int>>--二维整型数组
+    ifstream Infile("insert.txt");
+    vector<string> vec_str; //vec_str数组用于存储从data.txt文件中读取的每一行的内容--每一行的内容都是字符串
+    vector<vector<string> > vec_substr; //vec_substr数组用于存储对vec数组中每一行分割过后的内容
+    vector<vector<int> > vec_result;  //vec2用于存储将vec_substr中每个字符串转换为对应整数的形式
+    string s; //字符串s用于存储从.txt文件中读取的每行内容
+    while(getline(Infile,s)) //按行读取txt文件的内容--读取后的每行内容是字符串，存于vec_str中，
+    {
+        vec_str.push_back(s);
+    }
+    
+    for(int i=0; i<vec_str.size(); i++) //对读取进来的每行原始字符串进行分割
+    {
+        string s;
+        vec_substr.push_back({});
+        vec_substr[vec_substr.size()-1] = split(vec_str[i], " "); //对vec数组的每一行按空格进行分割,原vec中每行字符串会被分割为多个字符串
+
+    }
+
+    for(int i=0; i<vec_substr.size(); i++) //将vec_substr中每个字符串转换为对应的整数
+    {
+        vec_result.push_back({});
+        for(int j=0; j<vec_substr[i].size(); j++)
+        {
+            char * strs = new char[vec_substr[i][j].length() + 1] ;//vec1中每个元素都是string类型的，下面要调用的库函数atoi的接口要求其传入参数是C类型的字符串  
+            strcpy(strs, vec_substr[i][j].c_str());
+            vec_result[vec_result.size()-1].push_back(atoi(strs)); //调用库函数atoi
+        }
+    }
+    return vec_result;
+}
+
 int main()
 {
+
+
     RBTree T;
-    vector<int> Input = {12, 1, 9, 2, 0, 11, 7};
-    // vector<int> Input = {12, 1, 9, 2, 0, 11, 7, 19, 4, 15, 18, 5, 14, 13, 10, 16, 6, 3, 8, 17};
+    //  vector<int> Input = {12, 1, 9, 2, 0, 11, 7};
+    // // vector<int> Input = {12, 1, 9, 2, 0, 11, 7, 19, 4, 15, 18, 5, 14, 13, 10, 16, 6, 3, 8, 17};
+    vector<vector<int> > In = Read();
+
+    vector<int> Input;
+    int size = In[0][0];
+    for(int i=0; i<size; i++)
+    {
+        Input.push_back(In[1][i]);
+    }
+
     for(int i=0; i<Input.size(); i++)
     {
         TNode* p = CreatNode(Input[i]);
